9b1b2afa7dbaca7f6fc699e0e4b20be7422b417d
Raayush70 <61937218+Raayush70@users.noreply.github.com>
2021-09-22 20:41:28 +0530

    Added Homepage files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Homepage from "./homepage.component";
+
+class App extends React.Component {
+    render() {
+        return <div><Homepage /></div>
+    }
+}
+
+export default App;
