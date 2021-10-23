e28e9b73e59457fa67fa10ba250e9afa0a98ace9
Aayush363 <aayushkgp2018@gmail.com>
2021-10-24 00:45:12 +0530

    Added the some files which were not commited

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,8 +4,10 @@ import { connect } from 'react-redux';
 import { ReactComponent as Logo } from './crown.svg';
 import { auth } from '../firebase/firebase.utility';
 import './header.styles.scss';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
     return (
         <div className='header'>
             <div className='logo-container'>
@@ -34,11 +36,18 @@ const Header = ({ currentUser }) => {
                             SIGN IN
                         </Link>
                 }
+                <CartIcon />
             </div>
+            {
+                hidden ?
+                    null : <CartDropDown />
+
+            }
         </div>
     )
 }
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 });
 export default connect(mapStateToProps)(Header);
